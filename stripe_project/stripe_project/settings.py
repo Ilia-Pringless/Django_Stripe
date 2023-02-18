07ff00db40363a30572abfe58be9e4616a9892f2
Iliukuhin Ilia <iiliukhin@gmail.com>
2023-02-18 19:37:40 +0400

    added path in readme.md

--- stripe_project/stripe_project/settings.py
+++ stripe_project/stripe_project/settings.py
@@ -9,11 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'web',
+    '18.183.35.85'
 ]
 
 
