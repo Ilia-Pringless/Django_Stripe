1767c86242cca09ac840387f381411d2a0e6d569
Iliukuhin Ilia <iiliukhin@gmail.com>
2023-02-18 18:12:00 +0400

    any fix, added secrets

--- stripe_project/payments/urls.py
+++ stripe_project/payments/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("buy/<int:id>", views.send_order_payment, name="buy_item"),
-    path("buy_order/<int:id>", views.send_order_payment, name="buy_order"),
-    path("item/<int:item_id>", views.ItemPageView.as_view(), name="item"),
-    path("order/<int:order_id>", views.OrderPageView.as_view(), name="order"),
+    path('buy/<int:id>', views.send_order_payment, name='buy_item'),
+    path('buy_order/<int:id>', views.send_order_payment, name='buy_order'),
+    path('item/<int:item_id>', views.ItemPageView.as_view(), name='item'),
+    path('order/<int:order_id>', views.OrderPageView.as_view(), name='order'),
 ]
